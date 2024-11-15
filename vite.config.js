dce786920902fe340526a4468b9b9a812714d315
nirav-jivani <niravjivani82@gmail.com>
2024-11-15 18:35:34 -0500

    fommt

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://nirav-jivani.github.io/nirav-jivani/',
+  base:'https://nirav-jivani.github.io/3d_portfolio/',
   plugins: [react()],
 })
